db1ec11e1a2df1e0b374b015e8787820a075b584
Alexandre Londe <alexandrelonde.x@gmail.com>
2023-05-02 19:48:38 -0300

    feat: authenticate and enviroment

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  login(): void {
+    this.authService.authenticate(this.username, this.password).subscribe(
+      (token) => {
+        this.authService.setToken(token);
+        this.router.navigate(['/buscar']);
+      }
+    );
+  }
 
 }
