b490915957663af34e63bf8ee56b7c83d0cc0883
Alexandre Londe <alexandrelonde.x@gmail.com>
2023-05-02 10:55:22 -0300

    feat: add route of login and buscar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { BuscaLivreComponent } from './components/busca-livre/busca-livre.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'buscar', component: BuscaLivreComponent},
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
